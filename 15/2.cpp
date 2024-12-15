3af598f6d334718c650054381d548a425ad6ac7f
dorianturner <dorianemmanuelturner@gmail.com>
2024-12-15 23:08:12 +0000

    "Borrowed" KOLESNiii solution. In retrospect, I was just being special, a few kinks but mainly indexing was the wrong way around :(, why didn't it matter in p1?

--- 15/2.cpp
+++ 15/2.cpp
@@ -2,143 +2,117 @@
 using namespace std;
 typedef long long ll;
 
+bool canMoveBlocks(int dx, int dy, ll cx, ll cy, vector<string>& map) {
+    if (map[cy][cx] == '#') {
+        return false;
+    } else if (map[cy][cx] == '.') {
+        return true;
+    } else {
+        if (dy != 0) { // The vertical case
+            if (map[cy][cx] == '[') {
+                return canMoveBlocks(dx, dy, cx, cy + dy, map) && canMoveBlocks(dx, dy, cx + 1, cy + dy, map);
+            } else {
+                return canMoveBlocks(dx, dy, cx, cy + dy, map) && canMoveBlocks(dx, dy, cx - 1, cy + dy, map);
+            }
+        } else {
+            return canMoveBlocks(dx, dy, cx + dx, cy + dy, map);
+        }
+    }
+}
+
+void moveBlocks(int dx, int dy, ll cx, ll cy, vector<string>& map) {
+    if (map[cy][cx] == '.') {
+        return;
+    }
+    if (dy != 0) { // The vertical case for the next one
+        if (map[cy][cx] == '[') {
+            moveBlocks(dx, dy, cx + 1, cy + dy, map);
+            map[cy + dy][cx + 1] = map[cy][cx + 1];
+            map[cy][cx + 1] = '.';
+        } else {
+            moveBlocks(dx, dy, cx - 1, cy + dy, map);
+            map[cy + dy][cx - 1] = map[cy][cx - 1];
+            map[cy][cx - 1] = '.';
+        }
+    }
+    moveBlocks(dx, dy, cx + dx, cy + dy, map);
+    map[cy + dy][cx + dx] = map[cy][cx];
+    map[cy][cx] = '.';
+}
+
 ll gpssum(vector<string>& map) {
     ll sum = 0;
-    for (int i = 0; i < map.size(); ++i) {
-        for (int j = 0; j < map[i].size(); ++j) {
-            if (map[i][j] == '[') {
-                sum += (100 * i) + j;
+    for (ll y = 0; y < map.size(); ++y) {
+        for (ll x = 0; x < map[0].size(); ++x) {
+            if (map[y][x] == '[') {
+                sum += 100 * y + x;
             }
         }
     }
     return sum;
 }
 
-bool canMoveBlocks(int dx, int dy, int x, int y, vector<string>& map) {
-    if (map[y][x] == '#') {
-        return false;
-    } else if (map[y][x] == '.') {
-        return true;
-    } else {
-        if (dy != 0) {
-            if (map[y][x] == '[') {
-                return canMoveBlocks(dx, dy, x, y + dy, map) && canMoveBlocks(dx, dy, x + 1, y + dy, map);
-            } else {
-                return canMoveBlocks(dx, dy, x, y + dy, map) && canMoveBlocks(dx, dy, x - 1, y + dy, map);
-            }
-        } else {
-            return canMoveBlocks(dx, dy, x + dx, y + dy, map);
-        }
-    }
-}
-
-void moveBlocks(int dx, int dy, int x, int y, vector<string>& map) {
-    if (map[y][x] == '.') {
-        return;
-    }
-    if (dy != 0) {
-        if (map[y][x] == '[') {
-            moveBlocks(dx, dy, x + 1, y + dy, map);
-            map[y + dy][x + 1] = map[y][x + 1];
-            map[y][x + 1] = '.';
-        } else {
-            moveBlocks(dx, dy, x - 1, y + dy, map);
-            map[y + dy][x - 1] = map[y][x - 1];
-            map[y][x - 1] = '.';
-        }
-    }
-    moveBlocks(dx, dy, x + dx, y + dy, map);
-    map[y + dy][x + dx] = map[y][x];
-    map[y][x] = '.';
-}
-
-void simulate(vector<string>& map, pair<int, int>& rpos, char m) {
-    vector<pair<int, int>> directions = {
-        {-1, 0}, // Up
-        {0, 1},  // Right
-        {1, 0},  // Down
-        {0, -1}  // Left
-    };
-
-    // Printing map for debugging
-    for (int i = 0; i < map.size(); ++i) {
-        for (int j = 0; j < map[i].size(); ++j) {
-            cout << map[i][j];
-        }
-        cout << endl;
-    }
-
-    int dir_index = 0;
-    if (m == '^') dir_index = 0;
-    else if (m == '>') dir_index = 1;
-    else if (m == 'v') dir_index = 2;
-    else if (m == '<') dir_index = 3;
-
-    int dx = directions[dir_index].first;
-    int dy = directions[dir_index].second;
-
-    int new_x = rpos.first + dx;
-    int new_y = rpos.second + dy;
-
-    char target = map[new_y][new_x];
-
-    if (target == '#') {
-        return;
-    } else if (target == '.') {
-        swap(map[rpos.second][rpos.first], map[new_y][new_x]);
-        rpos = {new_x, new_y};
-    } else if (canMoveBlocks(dx, dy, new_x, new_y, map)) {
-        moveBlocks(dx, dy, new_x, new_y, map);
-        swap(map[rpos.second][rpos.first], map[new_y][new_x]);
-        rpos = {new_x, new_y};
-    }
-
-    return;
-}
-
 int main() {
-    ifstream fin("test_map.in");
-    ifstream sfin("test_moves.in");
-
-    vector<string> map;
+    ifstream inputFile("input.in");
+    vector<pair<int, int>> moves;
     string line;
-    while (getline(fin, line)) {
-        string new_line;
-        for (char c : line) {
-            if (c == '#') new_line += "##";
-            else if (c == 'O') new_line += "[]";
-            else if (c == '.') new_line += "..";
-            else if (c == '@') new_line += "@.";
-        }
-        map.push_back(new_line);
-    }
-
-    // Find robot pos
-    int x = -1, y = -1;
-    for (int i = 0; i < map.size(); ++i) {
-        for (int j = 0; j < map[i].size(); ++j) {
-            if (map[i][j] == '@') {
-                x = i;
-                y = j;
-                break;
+    vector<string> map;
+    ll cx, cy;
+    bool isMap = true;
+    int y = 0;
+    
+    while (getline(inputFile, line)) {
+        if (line.empty()) {
+            isMap = false;
+        } else if (isMap) {
+            string temp = "";
+            for (int x = 0; x < line.size(); ++x) {
+                if (line[x] == '@') {
+                    cx = 2 * x; // We multiply by 2 to adjust for doubled-width map
+                    cy = y;
+                    temp += "@.";
+                } else if (line[x] == 'O') {
+                    temp += "[]";
+                } else if (line[x] == '.') {
+                    temp += "..";
+                } else {
+                    temp += "##";
+                }
+            }
+            map.push_back(temp);
+        } else {
+            for (char m : line) {
+                switch (m) {
+                    case '<': moves.push_back({-1, 0}); break;
+                    case '^': moves.push_back({0, -1}); break;
+                    case '>': moves.push_back({1, 0}); break;
+                    case 'v': moves.push_back({0, 1}); break;
+                    default: break;
+                }
             }
         }
+        ++y;
     }
 
-    pair<int, int> rpos = {x, y};
+    pair<int, int> rpos = {cx, cy};
 
-    char m;
-    while (sfin >> m) {
-        simulate(map, rpos, m);
-    }
-
-    // Print map for debugging
-    for (int i = 0; i < map.size(); ++i) {
-        for (int j = 0; j < map[i].size(); ++j) {
-            cout << map[i][j];
+    for (auto [dx, dy] : moves) {
+        if (canMoveBlocks(dx, dy, rpos.first + dx, rpos.second + dy, map)) {
+            moveBlocks(dx, dy, rpos.first + dx, rpos.second + dy, map);
+            map[rpos.second][rpos.first] = '.';
+            map[rpos.second + dy][rpos.first + dx] = '@';
+            rpos.first += dx;
+            rpos.second += dy;
         }
-        cout << endl;
     }
 
-    cout << gpssum(map) << endl;
+    ll s = gpssum(map);
+
+    // Print the map
+    // for (const auto& row : map) {
+    //     cout << row << endl;
+    // }
+
+    cout << s << endl;
+    return 0;
 }
